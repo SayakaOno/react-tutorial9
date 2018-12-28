0b05790254cbcd732d7e18874353aa31bb1345f4
Sayaka Ono <sayaka.ono.81@gmail.com>
2018-12-28 14:42:53 -0800

    implement delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,6 +10,10 @@ class Counter extends React.Component {
     count: 0
   };
 
+  componentDidMount = () => {
+    this.setState({ count: this.props.counter.value });
+  };
+
   handleIncrement() {
     this.setState(state => {
       return { count: state.count + 1 };
@@ -25,6 +29,12 @@ class Counter extends React.Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
