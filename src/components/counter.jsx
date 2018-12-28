9b574cf8a20b540cbe94f3ca6bfcfe7bf9735ddf
Sayaka Ono <sayaka.ono.81@gmail.com>
2018-12-28 13:56:57 -0800

    * add bootstrap * add logic for counter

---
+++ src/components/counter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: 2
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
+}
+
+export default Counter;
